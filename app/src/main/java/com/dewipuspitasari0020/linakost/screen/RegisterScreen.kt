8479fd7f7487a308fab48be6993913f74ff9d338
DewiPuspitass <145330132+DewiPuspitass@users.noreply.github.com>
2025-06-24 19:29:36 +0700

    menambahkan dependencies gps

--- app/src/main/java/com/dewipuspitasari0020/linakost/screen/RegisterScreen.kt
+++ app/src/main/java/com/dewipuspitasari0020/linakost/screen/RegisterScreen.kt
@@ -1,5 +1,10 @@
 package com.dewipuspitasari0020.linakost.screen
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -21,6 +26,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -28,9 +34,12 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat.requestLocationUpdates
 import com.dewipuspitasari0020.linakost.ui.theme.bg
 import com.dewipuspitasari0020.linakost.ui.theme.bgSecondary
 import com.dewipuspitasari0020.linakost.ui.theme.textBlack
+import com.google.android.gms.location.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,8 +48,37 @@ fun RegisterScreen() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
+    var longitude by remember { mutableStateOf("") }
+    var latitude by remember { mutableStateOf("") }
     var passwordVisibility by remember { mutableStateOf(false) }
-    var repeatPasswordVisibility by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    val locationCallback = remember {
+        object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    latitude = location.latitude.toString()
+                    longitude = location.longitude.toString()
+                    Log.d("Location", "Latitude: $latitude, Longitude: $longitude")
+                    fusedLocationClient.removeLocationUpdates(this)
+                }
+            }
+        }
+    }
+
+    val locationPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val granted = permissions.entries.all { it.value }
+        if (granted) {
+            Log.d("Location", "Izin lokasi diberikan.")
+            requestLocationUpdates(fusedLocationClient, locationCallback, context)
+        } else {
+            Log.d("Location", "Izin lokasi ditolak.")
+        }
+    }
 
     Scaffold(
         containerColor = bg
@@ -182,6 +220,81 @@ fun RegisterScreen() {
                     ),
                     modifier = Modifier.fillMaxWidth()
                 )
+                Spacer(modifier = Modifier.height(32.dp))
+                OutlinedTextField(
+                    value = longitude,
+                    onValueChange = { longitude = it },
+                    label = { Text("Longitude", color = Color.Gray) },
+                    readOnly = true, // Membuat field tidak bisa diedit manual
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.DarkGray,
+                        cursorColor = Color.White,
+                        focusedLabelColor = Color.LightGray,
+                        unfocusedLabelColor = Color.Gray,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        containerColor = Color.Transparent
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Input Latitude
+                OutlinedTextField(
+                    value = latitude,
+                    onValueChange = { latitude = it },
+                    label = { Text("Latitude", color = Color.Gray) },
+                    readOnly = true, // Membuat field tidak bisa diedit manual
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.DarkGray,
+                        cursorColor = Color.White,
+                        focusedLabelColor = Color.LightGray,
+                        unfocusedLabelColor = Color.Gray,
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White,
+                        containerColor = Color.Transparent
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        when {
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED -> {
+                                requestLocationUpdates(fusedLocationClient, locationCallback, context)
+                            }
+                            else -> {
+                                locationPermissionLauncher.launch(
+                                    arrayOf(
+                                        Manifest.permission.ACCESS_FINE_LOCATION,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION
+                                    )
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = bgSecondary,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "Get Current Location",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
                 Spacer(modifier = Modifier.height(32.dp))
 
                 Button(
